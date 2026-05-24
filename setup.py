31614917ad47393fd725ed990c6a089d47e66c85
Ador <mkador169@gmail.com>
2026-05-24 08:38:09 +0600

    setup added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="shipment",
+    version="0.0.1",
+    author="mkador",
+    author_email="mkador169@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
